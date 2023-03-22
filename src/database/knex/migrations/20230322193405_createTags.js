e1e331f000fc25fa5125802182af57b3445f8abd
Daiane Araujo <daiane.ajmachado@gmail.com>
2023-03-22 16:45:12 -0300

    create migrations entities

---
+++ src/database/knex/migrations/20230322193405_createTags.js
@@ -0,0 +1,8 @@
+exports.up = knex => knex.schema.createTable("tags", table => {
+  table.increments("id");
+  table.text("movie_id").references("id").inTable("movies").onDelete("CASCADE");
+  table.text("user_id").references("id").inTable("users");
+  table.text("name").notNullable();
+});
+
+exports.down = knex => knex.schema.dropTable("tags");

0178c0d85308856f8e4a148a50e7cf08116c095c
Daiane Araujo <daiane.ajmachado@gmail.com>
2023-06-21 16:33:47 -0300

    add email and password validation

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,12 @@ const { Router } = require("express");
 const usersRouter = require("./users.routes");
 const movieNotesRouter = require("./movies.routes");
 const movieTagsRouter = require("./tags.routes");
+const sessionsRouter = require("./sessions.routes");
 
 const routes = Router();
 
 routes.use("/users", usersRouter);
+routes.use("/sessions", sessionsRouter);
 routes.use("/movies", movieNotesRouter);
 routes.use("/tags", movieTagsRouter);
 

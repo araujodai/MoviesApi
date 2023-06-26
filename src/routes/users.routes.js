802c62a90973d15f6398b15a6cb426097e2183c1
Daiane Araujo <daiane.ajmachado@gmail.com>
2023-06-26 16:38:27 -0300

    feat: add image loading to users route

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,13 +1,20 @@
-const { Router } = require("express");
+const { Router, response } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
 const UsersController = require("../controllers/UsersController");
 const ensureAuthentication = require("../middlewares/ensureAuthentication");
 
 const usersRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController();
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthentication, usersController.update);
+usersRoutes.patch("/avatar", ensureAuthentication, upload.single("avatar"), (request, response) => {
+  console.log(request.file.filename);
+  response.json();
+});
 
 module.exports = usersRoutes;

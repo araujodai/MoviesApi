f9ba1019b875a0ca4a6d15669bf6fa69a6774bb6
Daiane Araujo <daiane.ajmachado@gmail.com>
2023-06-26 17:43:19 -0300

    feat: save avatar in database

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,20 +1,19 @@
-const { Router, response } = require("express");
+const { Router } = require("express");
 const multer = require("multer");
 const uploadConfig = require("../configs/upload");
 
 const UsersController = require("../controllers/UsersController");
+const UserAvatarController = require("../controllers/UserAvatarController");
 const ensureAuthentication = require("../middlewares/ensureAuthentication");
 
 const usersRoutes = Router();
 const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthentication, usersController.update);
-usersRoutes.patch("/avatar", ensureAuthentication, upload.single("avatar"), (request, response) => {
-  console.log(request.file.filename);
-  response.json();
-});
+usersRoutes.patch("/avatar", ensureAuthentication, upload.single("avatar"), userAvatarController.update);
 
 module.exports = usersRoutes;

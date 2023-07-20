6a34a8f3f30ea0d6b8480d40bf0fdba02a7ab211
Daiane Araujo <daiane.ajmachado@gmail.com>
2023-07-20 13:27:03 -0300

    style: review code

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,6 @@ const { Router } = require("express");
 
 const usersRouter = require("./users.routes");
 const movieNotesRouter = require("./movies.routes");
-const movieTagsRouter = require("./tags.routes");
 const sessionsRouter = require("./sessions.routes");
 
 const routes = Router();
@@ -10,6 +9,5 @@ const routes = Router();
 routes.use("/users", usersRouter);
 routes.use("/sessions", sessionsRouter);
 routes.use("/movies", movieNotesRouter);
-routes.use("/tags", movieTagsRouter);
 
 module.exports = routes;
